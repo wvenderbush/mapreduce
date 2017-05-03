d13a813e42e74c54c1d8bad017394952a66931f5
Costas100 <constantineathanitis@yahoo.com>
2017-05-02 23:23:44 -0400

    finished single word frequency

--- func.py
+++ func.py
@@ -21,4 +21,17 @@ book = open("book_example.txt","r")
 book_text = book.read()
 
 book_words = book_text.split();
-print book_words
+
+'''print book_words
+print
+print "---------------------------------------------------"
+print
+'''
+
+def f_single(word):
+   list_single_word = filter(lambda x: x == word, book_words)
+   #return list_single_word
+   return len(list_single_word)
+
+
+print f_single("a")
