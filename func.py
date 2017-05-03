efb76384161bf6ff1a8f5d17be5eb5c48dbdbe35
Costas100 <constantineathanitis@yahoo.com>
2017-05-02 23:10:12 -0400

    got list of words

--- func.py
+++ func.py
@@ -1,10 +1,9 @@
-
 import cgi
 import cgitb
 import random
 #cgitb.enable()
 
-d = {}
+'''d = {}
 orig = cgi.FieldStorage()
 for k in orig.keys():
     d[k] = orig[k].value
@@ -14,3 +13,12 @@ x = d["book"]
 inSight = open(str(x)+".htm", "r")
 book = inSight.read()
 inSight.close()
+'''
+
+#book = open("book.txt","r")
+
+book = open("book_example.txt","r")
+book_text = book.read()
+
+book_words = book_text.split();
+print book_words
