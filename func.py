557d1d5b72c9db80c5b091a442c922b3475224a3
Costas100 <constantineathanitis@yahoo.com>
2017-05-03 00:17:29 -0400

    finished most frequent

--- func.py
+++ func.py
@@ -30,14 +30,28 @@ print
 
 def f_single(word):
    list_single_word = filter(lambda x: x == word, book_words)
-   #return list_single_word
+  
    return len(list_single_word)
 
 def f_phrase(words):
-	list_words = filter(lambda x: x in words, book_words)
-	return len(list_words)
+   list_words = filter(lambda x: x in words, book_words)
+   return len(list_words)
+
+
+def most_frequent():
+   word = reduce( lambda x, y: x if f_single(x) >= f_single(y) else y, book_words )
+   return word
+
+
 
 
 print f_single("a")
+print f_single("the")
+print f_phrase(["a","the"])
 
+print f_single("we")
+print f_single("do")
+print f_single("not")
 print f_phrase(["we", "do", "not"])
+
+print most_frequent()
